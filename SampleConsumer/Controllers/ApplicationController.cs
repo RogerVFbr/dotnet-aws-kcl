eae95351af1f9f7edcd0007a90f6e9ecd28d55b1
RogerVFbr <roger.freret@al.infnet.edu.br>
2024-01-11 17:20:26 -0300

    Final home updates

--- SampleConsumer/Controllers/ApplicationController.cs
+++ SampleConsumer/Controllers/ApplicationController.cs
@@ -3,32 +3,31 @@ using ClientLibrary;
 using ClientLibrary.inputs;
 using Microsoft.Extensions.Logging;
 
-namespace SampleConsumer.Controllers
-{
-    public class ApplicationController : ShardRecordsProcessorBase<ApplicationController>, IShardRecordProcessor
-    {
-        public ApplicationController(ILogger<ApplicationController> logger) : base(logger)
-        {
-        }
-        
-        protected override void Process(ProcessRecordsInput input)
-        {
-            foreach (var rec in input.Records)
-            {
-                var data = string.Empty;
-                
-                try
-                {
-                    data = System.Text.Encoding.UTF8.GetString(rec.Data);
-                    _logger.LogInformation("IMIN");
-                    _logger.LogInformation(data);
+namespace SampleConsumer.Controllers;
 
-                    // Your own logic to process a record goes here.
-                }
-                catch (Exception e)
-                {
-                    _logger.LogError(e, "Exception processing record data: {Data}", data);
-                }
+public class ApplicationController : ShardRecordsProcessorBase<ApplicationController>, IShardRecordProcessor
+{
+    public ApplicationController(ILogger<ApplicationController> logger) : base(logger)
+    {
+    }
+    
+    protected override void Process(ProcessRecordsInput input)
+    {
+        foreach (var rec in input.Records)
+        {
+            var data = string.Empty;
+            
+            try
+            {
+                data = System.Text.Encoding.UTF8.GetString(rec.Data);
+                _logger.LogInformation("IMIN");
+                _logger.LogInformation(data);
+
+                // Your own logic to process a record goes here.
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Exception processing record data: {Data}", data);
             }
         }
     }
